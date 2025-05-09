a5d8d549dc128fdf579fd8195fddac638b5b7b3e
Rania Raafat <mohrealrafet@gmail.com>
2025-05-09 19:52:18 +0300

    Update to problem1b.c

--- problem1b.c
+++ problem1b.c
@@ -20,28 +20,35 @@ int main(int argc, char* argv[]) {
     MPI_Status status;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    double startTime = MPI_Wtime();
 
-    int x=1, y=16;
+    int x, y;
     if (rank == 0) {
-
-        int total = y - x ;
+        printf("enter x: \n");
+        scanf("%d", &x);
+        printf("enter y: \n");
+        scanf("%d", &y);
+        int total = y - x+1;
         int subrange = total / (size - 1);
-        int remainder = total % (size - 1);
+        //int remainder = total % (size - 1);
 
         for (int i = 1; i < size; i++) {
             MPI_Send(&x, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&subrange, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&remainder, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&y, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
     }
 
-    if(rank!=0) {
-        int start, subrange, remainder;
+    if (rank != 0) {
+        int start, subrange, end;
         MPI_Recv(&start, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&subrange, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(&remainder, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&end, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
+        int total = end - start+1;
+        int remainder = total % (size - 1);
         int id = rank - 1;
+        
         int a = start + id * subrange + (id < remainder ? id : remainder);
         int b = a + subrange - 1;
 
@@ -51,8 +58,8 @@ int main(int argc, char* argv[]) {
 
         size_t range = (size_t)(b - a + 1);
         int* subArray = (int*)malloc(range * sizeof(int));
-        
-     
+
+
         int subCount = 0;
         for (int i = a; i <= b; i++) {
             if (is_prime(i)) {
@@ -76,47 +83,49 @@ int main(int argc, char* argv[]) {
 
         free(subArray);
     }
-    MPI_Barrier(MPI_COMM_WORLD);           //lw 3ayza p0 tprint akher haga fehom
 
-        if (rank == 0) {
-            int total = y - x;
+    if (rank == 0) {
+        int total = y - x;
 
-            int* allPrimes = (int*)malloc(total * sizeof(int));
+        int* allPrimes = (int*)malloc(total * sizeof(int));
 
-            int pos = 0;
-            int total_count = 0;
+        int pos = 0;
+        int total_count = 0;
 
-            for (int i = 1; i < size; i++) {
-                int subcount = 0;
+        for (int i = 1; i < size; i++) {
+            int subcount = 0;
 
-                MPI_Recv(&subcount, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+            MPI_Recv(&subcount, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
 
-                int* subarray = (int*)malloc(subcount * sizeof(int));
+            int* subarray = (int*)malloc(subcount * sizeof(int));
 
-                MPI_Recv(subarray, subcount, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
+            MPI_Recv(subarray, subcount, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
 
 
-                for (int j = 0; j < subcount; j++) {
-                    allPrimes[pos++] = subarray[j];
-                }
-
-                total_count += subcount;
-                free(subarray);
+            for (int j = 0; j < subcount; j++) {
+                allPrimes[pos++] = subarray[j];
             }
 
-
-            printf("After reduction, P0 will have Count = %d (", total_count);
-
-            for (int i = 0; i < total_count; i++) {
-                printf("%d, ", allPrimes[i]);
-            }
-            printf(")\n");
-
-            free(allPrimes);
-
-
+            total_count += subcount;
+            free(subarray);
         }
 
+
+        printf("After reduction, P0 will have Count = %d (", total_count);
+
+        for (int i = 0; i < total_count; i++) {
+            printf("%d ", allPrimes[i]);
+        }
+        printf(")\n");
+
+        free(allPrimes);
+    double endTime = MPI_Wtime();
+    printf("Execution time: %f sec \n", endTime - startTime);
+
+    }
+
     MPI_Finalize();
+   
+
     return 0;
 }
